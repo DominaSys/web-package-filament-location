1c31f17058dc9255a5f024696c1751fda7e1bcce
Bruno Mangilli <nobrudevz@gmail.com>
2026-07-07 09:38:24 -0300

    chore: remover placeholders de campos de localização - eliminar textos de placeholder que não eram mais necessários - manter apenas os rótulos exibidos nos campos

--- resources/lang/en/location.php
+++ resources/lang/en/location.php
@@ -5,31 +5,24 @@ return [
     'fields' => [
         'postal_code' => [
             'label' => 'Postal code',
-            'placeholder' => '00000-000',
         ],
         'state' => [
             'label' => 'State',
-            'placeholder' => 'Select a state',
         ],
         'city' => [
             'label' => 'City',
-            'placeholder' => 'Select a city',
         ],
         'neighborhood' => [
             'label' => 'Neighborhood',
-            'placeholder' => 'Neighborhood',
         ],
         'street' => [
             'label' => 'Street',
-            'placeholder' => 'Street',
         ],
         'number' => [
             'label' => 'Number',
-            'placeholder' => 'Number',
         ],
         'complement' => [
             'label' => 'Complement',
-            'placeholder' => 'Complement',
         ],
     ],
 
